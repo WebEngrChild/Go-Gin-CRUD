0321103afb29f7d96c7a49eb679cf2809ba6528f
joe-main <qmmplusfact@gmail.com>
2022-08-11 09:30:26 +0900

    ホットリロード環境

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello golang from docker!")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+		})
+	})
+
+	r.Run(":3000")
 	//router := routes.NewRoutes()
 	//router.Run()
 }
